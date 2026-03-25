4fd893a79f07f630e2d1f169def561320de7ad56
kriv <kevinorlandorivera0@gmail.com>
2026-03-25 07:24:42 -0500

    feat: project initialization and database configuration

---
+++ src/main/java/com/academy/kevin/KevinApplication.java
@@ -0,0 +1,13 @@
+package com.academy.kevin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KevinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KevinApplication.class, args);
+	}
+
+}
